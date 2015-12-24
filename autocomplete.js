a8154107ba3ef5e18a81d3e113620805efed0233
Jeffrey Yang <jeffycyang@gmail.com>
2015-12-23 17:02:47 -0800

    fetches data from urban dictionary api but doesn't yet display

--- autocomplete.js
+++ autocomplete.js
@@ -7,7 +7,7 @@ if (document.querySelectorAll)
 else
 {
     textInputs = [];
-    var unfiltered = document.getElementsByTagName("input"),
+    var unfiltered = document.getElementsByTagName('input'),
         i = unfiltered.length,
         input;
     while(i--)
@@ -20,22 +20,37 @@ else
     }
 }
 
-var awesomplete = new Awesomplete(input, {
+$(textInputs).addClass('awesomplete');
+
+var awesomplete = new Awesomplete(textInputs[0], {
   minChars: 1,
   autoFirst: true
 });
 
 $(textInputs).on("keyup", function(){
-    $.ajax({
-      url: 'http://api.urbandictionary.com/v0/define?term=' + this.value,
-      type: 'GET',
-      dataType: 'json'
-    })
-    .success(function(data) {
-      var list = [];
-      $.each(data, function(key, value) {
-        list.push(value.tags);
-      });
-      awesomplete.list = list;
+    // $.ajax({
+    //   url: 'http://api.urbandictionary.com/v0/define?term=' + this.value,
+    //   type: 'GET',
+    //   dataType: 'json'
+    // })
+    // .success(function(data) {
+    //   var list = [];
+    //   console.log('success!');
+    //   $.each(data, function(key, value) {
+    //     list.push(value.tags);
+    //   });
+    //   awesomplete.list = list;
+    // });
+    chrome.runtime.sendMessage({
+        method: 'GET',
+        action: 'xhttp',
+        url: 'http://api.urbandictionary.com/v0/define?term=' + this.value
+    }, function(responseText) {
+        var list = [];  
+        $.each(JSON.parse(responseText)["tags"], function(key, value) {
+            console.log("choice "+value);
+            list.push(value);
+        });
+        awesomplete.list = list;
     });
 });
