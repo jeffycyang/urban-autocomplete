e64f634026a215a8078520932eedda339e0d4c53
Jeffrey Yang <jeffycyang@gmail.com>
2015-12-23 21:46:06 -0800

    works only forfirst input field, needs improvement

--- autocomplete.js
+++ autocomplete.js
@@ -21,39 +21,13 @@ console.log(textInputs);
 
 var awesomplete = new Awesomplete(textInputs[0], {
   minChars: 1,
-  autoFirst: true
+  autoFirst: true,
+  filter: function(){
+    return true;
+  }
 });
 
 $(textInputs).on("keyup", function() {
-    // THIS DOESN'T WORK BECAUSE OF HTTP on HTTPS SITES
-    // $.ajax({
-    //   url: 'http://api.urbandictionary.com/v0/define?term=' + this.value,
-    //   type: 'GET',
-    //   dataType: 'json'
-    // })
-    // .success(function(data) {
-    //   var list = [];
-    //   console.log('success!');
-    //   $.each(data, function(key, value) {
-    //     list.push(value.tags);
-    //   });
-    //   awesomplete.list = list;
-    // });
-
-    // THIS WORKS BUT ITS NOT THE API I WANT TO PING
-    // $.ajax({
-    //     url: 'https://restcountries.eu/rest/v1/name/' + this.value,
-    //     type: 'GET',
-    //     dataType: 'json'
-    // })
-    // .success(function(data) {
-    //     var list = [];
-    //     $.each(data, function(key, value) {
-    //       list.push(value.name);
-    //     });
-    //     awesomplete.list = list;
-    // });
-
     chrome.runtime.sendMessage({
         method: 'GET',
         action: 'xhttp',
@@ -61,10 +35,8 @@ $(textInputs).on("keyup", function() {
     }, function(responseText) {
         var list = [];  
         $.each(JSON.parse(responseText)["tags"], function(key, value) {
-            console.log("choice "+value);
             list.push(value);
         });
         awesomplete.list = list;
-        console.log(awesomplete);
-    });    
+    });
 });
