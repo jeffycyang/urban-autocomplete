c485b2df0166fd4ee243738ce95538ef5d15773f
Jeffrey Yang <jeffycyang@gmail.com>
2015-12-24 01:33:26 -0800

    works only for first input field, needs improvement

--- autocomplete.js
+++ autocomplete.js
@@ -31,10 +31,12 @@ $(textInputs).on("keyup", function() {
     chrome.runtime.sendMessage({
         method: 'GET',
         action: 'xhttp',
-        url: 'http://api.urbandictionary.com/v0/define?term=' + this.value
+        // url: 'http://api.urbandictionary.com/v0/define?term=' + this.value
+        url: 'http://api.urbandictionary.com/v0/autocomplete?key=ab71d33b15d36506acf1e379b0ed07ee&term=' + this.value
     }, function(responseText) {
         var list = [];  
-        $.each(JSON.parse(responseText)["tags"], function(key, value) {
+        // $.each(JSON.parse(responseText)["tags"], function(key, value) {
+        $.each(JSON.parse(responseText), function(key, value) {
             list.push(value);
         });
         awesomplete.list = list;
